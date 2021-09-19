14eb39e1ee661616c0383c1236deb3c56d0b12cb
mukesh <mukeshcgs@gmail.com>
2021-09-19 22:13:35 +0530

    steps added

--- src/components/SecHowStake.js
+++ src/components/SecHowStake.js
@@ -14,7 +14,7 @@ const SecHowStake = () => {
         <Container id="stake-sec">
             <Row>
                 <Col sm={12}>
-                    <h1>How can I stake ownership in trading tech that works for me?</h1>
+                    <h1>How can I <b>stake ownership in trading tech</b> that works for me?</h1>
                 </Col>
             </Row>
             <Row>
--- src/components/SecSteps.js
+++ src/components/SecSteps.js
@@ -16,8 +16,8 @@ const SecSteps = () => {
                 <Col sm={4}>
                     <div className="steps-card">
                         <span className="step-num">01</span>
-                        <h2>No logins required, access secured by MetaMask wallet.</h2>
-                        <h2> Then one click to authorize the connection to your brokerage account.</h2>
+                        <h3>No logins required, access secured by MetaMask wallet.</h3>
+                        <h3> Then one click to authorize the connection to your brokerage account.</h3>
                     </div>
 
                 </Col>
@@ -25,14 +25,14 @@ const SecSteps = () => {
                     <div className="steps-card">
                         <span className="step-num">02</span>
 
-                        <h2>Research and select investment strategies from the drop down menu. </h2><h2>The strategies historical data and back-testing data can give you an edge.</h2>
+                        <h3>Research and select investment strategies from the drop down menu. </h3><h3>The strategies historical data and back-testing data can give you an edge.</h3>
                     </div>
                 </Col>
                 <Col sm={4}>
                     <div className="steps-card">
                         <span className="step-num">03</span>
-                        <h2>
-                            Select your stock(s) and deploy your selected trading technology to your investment account with 1 click.</h2><h2> Automatically, trades from your selected strategies will be deployed into your account.</h2>
+                        <h3>
+                            Select your stock(s) and deploy your selected trading technology to your investment account with 1 click.</h3><h3> Automatically, trades from your selected strategies will be deployed into your account.</h3>
                     </div>
                 </Col>
             </Row>
