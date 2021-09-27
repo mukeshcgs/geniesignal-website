bd7c8c647b98184522c2c3eac04212d0fcf9190c
mukesh <mukeshcgs@gmail.com>
2021-09-27 14:33:28 +0530

    router fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,12 +10,8 @@ import {
 } from "react-router-dom";
 
 const propTypes = {};
-
 const defaultProps = {};
 
-/**
- * 
- */
 const Navigation = () => {
   return <Navbar collapseOnSelect expand="lg" bg="" variant="dark">
     <Navbar.Brand href="#home"><img height="35" src="media/img/genie-logo-inline.png" alt="Genie Signal" /></Navbar.Brand>
@@ -24,13 +20,16 @@ const Navigation = () => {
       <Nav className="mr-auto">
       </Nav>
       <Nav>
-        <Link to="/faq">FAQ</Link>
-        <Link to="/developers">developers</Link>
-        <Nav.Link href="/">Home</Nav.Link>
-        <Nav.Link href="/developers">Developers</Nav.Link>
+        <Link className="nav-link" to="/">Home</Link>
+        <Link className="nav-link" to="/developers">Developers</Link>
+        <Link className="nav-link" to="/faq">FAQ</Link>
+        {/* <Link className="nav-link" to="https://geniesignal.webflow.io/" target="_blank">Launch The App</Link> */}
         <Nav.Link href="https://geniesignal.webflow.io/" target="_blank">Launch The App</Nav.Link>
         <Nav.Link href="https://metamask.io/download" target="_blank">Download MetaMask</Nav.Link>
-        <Nav.Link href="/faq">FAQ</Nav.Link>
+        {/* <Link className="nav-link" to="https://metamask.io/download" target="_blank">Download MetaMask</Link> */}
+        {/* <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/developers">Developers</Nav.Link>
+        <Nav.Link href="/faq">FAQ</Nav.Link> */}
       </Nav>
     </Navbar.Collapse>
   </Navbar>;
